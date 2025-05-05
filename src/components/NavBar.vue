c1ab8136195ea9241bf1df81735087a3dbd99b03
ian.harris <flavious59@gmail.com>
2025-05-05 17:34:32 -0500

    Basic Nav Bar, some fiddling with CSS; Boy am I out of practice

---
+++ src/components/NavBar.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <nav class="nav-text">
+    <RouterLink to="/">Home</RouterLink> <span> | </span>
+    <RouterLink to="/Projects">Projects</RouterLink> <span> | </span>
+    <RouterLink to="/Skills">Skills</RouterLink>
+  </nav>
+  <br />
+</template>
+
+<style lang="css">
+.nav-text {
+  font-size: 1.25em;
+}
+</style>
