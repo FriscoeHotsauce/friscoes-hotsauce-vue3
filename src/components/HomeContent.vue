266fcbe7963dd9c77ecc338b6ab90bd051324e18
Ian Harris <friscoe.hotsauce@gmail.com>
2025-05-16 12:45:18 -0500

    Created new Home Content component, updated Home View to display it. Pretty basic with lorem ipsum text for now

---
+++ src/components/HomeContent.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <h1>Hi, I'm Ian</h1>
+    <h2 class="header-padding">Pragmatic, Language-Agnostic, Back-End Specialist</h2>
+    <br>
+    <p>Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.</p>
+</template>
+
+<style lang="css" scoped>
+
+.header-padding{
+   padding-top: .1em 
+}
+
+</style>
